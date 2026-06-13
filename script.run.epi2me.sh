9fc830b545a8283a45a85e252aed92636f5e2c58
Bogdan Tanasa <tanasa@gmail.com>
2026-06-13 15:18:40 -0700

    Update script.run.epi2me.sh

--- script.run.epi2me.sh
+++ script.run.epi2me.sh
@@ -9,3 +9,24 @@ nextflow run epi2me-labs/wf-alignment \
 --depth_coverage \
 --minimap_preset dna \
 --threads 12
+
+export NXF_VER=25.04.8
+
+nextflow run epi2me-labs/wf-single-cell \
+  --expected_cells 100 \
+  --fastq wf-single-cell-demo/chr17.fq.gz \
+  --kit 3prime:v3 \
+  --ref_genome_dir wf-single-cell-demo \
+  --genes_of_interest wf-single-cell-demo/umap_plot_genes.csv \
+  --out_dir nano_wf-single-cell \
+  --threads 8 \
+  --matrix_min_genes 200 \
+  --matrix_min_cells 3 \
+  --matrix_max_mito 20 \
+  --matrix_norm_count 10000 \
+  --stringtie_opts "-c 2" \
+  --estimate_cell_count true \
+  --call_variants true \
+  -c custom.config \
+  -profile standard \
+  -resume
